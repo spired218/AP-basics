5b708f68288c31b042f3739945df44a1d1006d10
spired218 <millert3373@masseyhigh.school.nz>
2024-11-06 12:20:27 +1300

    Number checker and gets width

---
+++ C 04 get the width.py
@@ -0,0 +1,13 @@
+# Ask useer for width and loop until they
+# enter a  number that is more than zero
+
+error = "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+        # ask the user for a number
+        width = float(input("width: "))
+
+        #check
+
+
